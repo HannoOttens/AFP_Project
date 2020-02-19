01e159003eb9f68b2dbcabeb449c2d12acd75986
Luuk Glorie <luukglorie@outlook.com>
2020-02-19 13:16:53 +0100

    basic static file server

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,24 @@
-module Main where
+{-# LANGUAGE DataKinds #-}
+--{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+--{-# LANGUAGE KindSignatures #-}
+
+import Data.Aeson
+import GHC.Generics
+import GHC.TypeLits
+import Network.Wai.Handler.Warp
+import Servant
+import Servant.Server.StaticFiles
+
+type API = StaticFile
+
+type StaticFile = Raw
+
+staticServer :: Server StaticFile
+staticServer = serveDirectoryWebApp "var/www"
+
+api :: Proxy API
+api = Proxy
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = run 8080 . serve api $ staticServer
