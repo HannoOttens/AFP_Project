47954d8f836e30f8c738f800bf99b4c638122fb9
Marcovdw <m.vandeweerthof@students.uu.nl>
2020-02-26 14:27:10 +0100

    Concat multiple tags

--- src/Scraper.hs
+++ src/Scraper.hs
@@ -1,33 +1,35 @@
 module Scraper (scrapeSite, scrapePage, scrapeElements, scrapeElement) where
 
-import Network.HTTP
+import Network.HTTP (getRequest, getResponseBody, simpleHTTP)
 import Text.HTML.TagSoup
 
 type Element = String
 type URL     = String
 
 -- Return all tags from all pages on a site
-scrapeSite :: [URL] -> IO [[Tag String]]
-scrapeSite = mapM scrapePage
+scrapeSite :: [URL] -> IO [Tag String]
+scrapeSite urls = do ts <- mapM scrapePage urls
+                     return $ concat ts
 
 -- Return all tags from a page
 scrapePage :: URL -> IO [Tag String]
 scrapePage = scrapeElement "html"
 
--- Return all tags within a designated elements from a url
-scrapeElements :: [Element] -> URL -> IO [[Tag String]]
-scrapeElements es url = mapM (`scrapeElement` url) es
+-- Return all tags within an elements from a url
+scrapeElements :: [Element] -> URL -> IO [Tag String]
+scrapeElements es url = do ts <- mapM (`scrapeElement` url) es
+                           return $ concat ts
 
--- Return all tags within a designated element from a url
+-- Return all tags within an element from a url
 scrapeElement :: Element -> URL -> IO [Tag String]
 scrapeElement e url = do tags <- parseTags <$> (getResponseBody =<< simpleHTTP (getRequest url))
                          return $ filterTags e 0 tags
 
 -- Filter tags such that they match the given element, counter to find nested elements with the same tag
 filterTags :: Element -> Int -> [Tag String] -> [Tag String]
-filterTags _ _ []     = [] -- End of tag
-filterTags e n (t:ts) | isTagOpenName  e t =     filterTags e (n + 1) ts -- <tag>, increase nesting level
-                      | isTagCloseName e t =     filterTags e (n - 1) ts -- </tag>, reduce nesting level
+filterTags _ _ []     = []                                               -- End of html
+filterTags e n (t:ts) | isTagOpenName  e t =     filterTags e (n + 1) ts -- <e>, increase nesting level
+                      | isTagCloseName e t =     filterTags e (n - 1) ts -- </e>, reduce nesting level
                       | isTagComment     t =     filterTags e n       ts -- ignore comment
-                      | n <= 0             =     filterTags e n       ts -- not inside designated tag
-                      | otherwise          = t : filterTags e n       ts -- inside designated tag, add tag
+                      | n <= 0             =     filterTags e n       ts -- not inside element
+                      | otherwise          = t : filterTags e n       ts -- inside element, add tag
