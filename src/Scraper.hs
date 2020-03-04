097b82b7d683e8a7f696555e4fd8d3a72861ed21
Marcovdw <m.vandeweerthof@students.uu.nl>
2020-03-04 11:52:22 +0100

    Hashed sites, scraper for attribute

--- src/Scraper.hs
+++ src/Scraper.hs
@@ -1,35 +1,43 @@
-module Scraper (scrapeSite, scrapePage, scrapeElements, scrapeElement) where
+module Scraper (scrapeSite, scrapePage, scrapeElements, scrapeElement, scrapeAttribute) where
 
 import Network.HTTP (getRequest, getResponseBody, simpleHTTP)
 import Text.HTML.TagSoup
+import Data.Hashable
 
-type Element = String
 type URL     = String
+type Element = String
+
+getSite :: URL -> IO String
+getSite url = getResponseBody =<< simpleHTTP (getRequest url)
 
 -- Return all tags from all pages on a site
-scrapeSite :: [URL] -> IO [Tag String]
-scrapeSite urls = do ts <- mapM scrapePage urls
-                     return $ concat ts
+scrapeSite :: [URL] -> IO [Int]
+scrapeSite = mapM scrapePage
 
 -- Return all tags from a page
-scrapePage :: URL -> IO [Tag String]
-scrapePage = scrapeElement "html"
+scrapePage :: URL -> IO Int
+scrapePage url = do site <- getSite url
+                    return $ hash site
 
--- Return all tags within an elements from a url
-scrapeElements :: [Element] -> URL -> IO [Tag String]
-scrapeElements es url = do ts <- mapM (`scrapeElement` url) es
-                           return $ concat ts
+-- Return all tags within all elements from a url
+scrapeElements :: [Element] -> URL -> IO [Int]
+scrapeElements es url = mapM (`scrapeElement` url) es
 
 -- Return all tags within an element from a url
-scrapeElement :: Element -> URL -> IO [Tag String]
-scrapeElement e url = do tags <- parseTags <$> (getResponseBody =<< simpleHTTP (getRequest url))
-                         return $ filterTags e 0 tags
+scrapeElement :: Element -> URL -> IO Int
+scrapeElement e url = do tags <- parseTags <$> getSite url
+                         return $ hash $ renderTags $ filterTags e ("", "") False 0 tags
 
--- Filter tags such that they match the given element, counter to find nested elements with the same tag
-filterTags :: Element -> Int -> [Tag String] -> [Tag String]
-filterTags _ _ []     = []                                               -- End of html
-filterTags e n (t:ts) | isTagOpenName  e t =     filterTags e (n + 1) ts -- <e>, increase nesting level
-                      | isTagCloseName e t =     filterTags e (n - 1) ts -- </e>, reduce nesting level
-                      | isTagComment     t =     filterTags e n       ts -- ignore comment
-                      | n <= 0             =     filterTags e n       ts -- not inside element
-                      | otherwise          = t : filterTags e n       ts -- inside element, add tag
+-- Return all tags within an element with a given attribute from a url
+scrapeAttribute :: Element -> Attribute String -> URL -> IO Int
+scrapeAttribute e a url = do tags <- parseTags <$> getSite url
+                             return $ hash $ renderTags $ filterTags e a False 0 tags
+
+-- Filter tags such that they match the given element and/or attribute, counter to find nested elements with the same tag
+filterTags :: Element -> Attribute String -> Bool -> Int -> [Tag String] -> [Tag String]
+filterTags _ _      _     _ []     = []
+filterTags e (k, v) False n (t:ts) = filterTags e (k, v) (isTagOpenName e t && (v == fromAttrib k t || k == "")) n ts -- <e k = v>, start collecting tags, otherwise continue with rest of tags
+filterTags e a      True  n (t:ts) | isTagOpenName  e t =     filterTags e a True     (n + 1) ts -- <e>, increase nesting level
+                                   | isTagCloseName e t =     filterTags e a (n == 1) (n - 1) ts -- </e>, reduce nesting level, end collecting if nesting is 0
+                                   | otherwise          = t : filterTags e a True     n       ts -- inside element, add tag
+
