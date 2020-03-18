6eeccf0c8e4ea1df6ae652fffbccb1c268c302b9
Luuk Glorie <luukglorie@outlook.com>
2020-03-18 18:12:19 +0100

    Test setup for web server testing

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,45 +1,33 @@
-import Test.Tasty
-import Test.Tasty.QuickCheck
-import Test.QuickCheck.Monadic
+
+import Network.Wai (Application)
+import Test.Hspec (Spec, hspec)
+import Test.Hspec.Wai(with)
 import Database.SQLite.Simple
 import Data.String(fromString)
 
+import App(app)
+import Config
 import qualified DBAdapter as DB
-import Models.User as UM
 
-genUser :: PropertyM IO User
-genUser = do
-    username <- pick arbitrary
-    password <- pick arbitrary
-    return $ User 0 username password 
+import AccountTest(accountTests)
 
-prop_insert_user :: Connection -> PropertyM IO ()
-prop_insert_user conn = do
-  user <- genUser
-  (Just personId) <- run $ DB.addUser user conn
-  let userWithID = user {UM.id = personId}
-  result <- run $ DB.getUser (UM.username user) conn
-  assert (result == Just userWithID)
+-- | specifications which use the server
+spec :: IO Application -> Spec
+spec app = with app $ do
+    accountTests
+    
 
+-- | Run tests with use of test database
 main :: IO ()
-main = withConnection ":memory:" $ \connection -> do
-  let 
-    runSqlProperty :: IO Property -> Property
-    runSqlProperty action = ioProperty $ do
-        prop <- action
-        resetDB connection
-        return prop
-
-    quickCheckSql :: String -> (Connection -> PropertyM IO ()) -> TestTree
-    quickCheckSql name prop = testProperty name $ monadic runSqlProperty (prop connection)
-
-  -- Initial DB setup code
-  initDB connection
-
-  -- Test as many quickcheck properties as you like
-  let prop = quickCheckSql "insert user" prop_insert_user
-  defaultMain prop
-
+main =
+    withConnection "test-db" $ \conn -> do
+        conf <- config
+        initDB conn
+        putStrLn "\napi running on port 8080..."
+        hspec $ spec $ do
+            -- Before running each test, clear all tables in the database
+            resetDB conn 
+            return $ app $ conf {dbFile = "test-db"}
 
 initDB :: Connection -> IO ()
 initDB conn = do 
@@ -50,4 +38,3 @@ resetDB :: Connection -> IO ()
 resetDB conn = do
     let tables = ["NotificationTokens", "Targets", "Websites", "Notifications", "Users"]
     mapM_ (execute_ conn . fromString . (++) "DELETE FROM ") tables
-
