07a24feac1647bc222e8fc56d5c3140892260d53
Luuk Glorie <luukglorie@outlook.com>
2020-03-18 18:11:54 +0100

    Decoupled main

---
+++ src/App.hs
@@ -0,0 +1,101 @@
+module App where
+
+import Network.Wai.Handler.Warp
+import Servant
+import Servant.Auth.Server
+import Servant.Auth.Server.SetCookieOrphan ()
+import Control.Monad
+import Control.Monad.Reader
+import Control.Monad.State
+import qualified Data.Hashable as H
+
+import qualified DBAdapter as DB
+import Handlers.Account
+import Handlers.Targets
+import Handlers.Notification
+import Config
+import Scraper
+import Models.Website
+import Models.User
+import Models.Target
+
+type PublicAPI = LoginAPI 
+type ProtectedAPI = TargetsAPI 
+               :<|> "notification" :> NotificationAPI
+type RawFiles = Raw
+type API = PublicAPI  
+            :<|> Servant.Auth.Server.Auth '[Cookie] User :> ProtectedAPI
+            :<|> RawFiles
+
+rawFiles :: ServerT RawFiles (AppConfig Handler)
+rawFiles = serveDirectoryWebApp "www"
+
+protected :: Servant.Auth.Server.AuthResult User -> ServerT ProtectedAPI (AppConfig Handler)    
+protected (Servant.Auth.Server.Authenticated user) = hoistServer protectedAPI (`evalStateT` user) (targetServer :<|> notificationServer)
+protected _ = throwAll err401
+
+public:: ServerT PublicAPI (AppConfig Handler)
+public = accountServer
+
+server :: ServerT API (AppConfig Handler)
+server = public 
+      :<|> protected 
+      :<|> rawFiles
+
+api :: Proxy API 
+api = Proxy
+
+protectedAPI :: Proxy ProtectedAPI
+protectedAPI = Proxy 
+
+authApi :: Proxy '[CookieSettings, JWTSettings]
+authApi = Proxy
+
+runApp :: Config -> IO ()
+runApp conf = run 8080 $ app conf
+
+app :: Config -> Application
+app conf = (serveWithContext api (authConf conf) $ hoistServerWithContext api authApi (`runReaderT` conf) server)
+
+pollWebsites :: AppConfig IO ()
+pollWebsites = do 
+      ws <- DB.exec DB.getWebsites
+      mapM_ pollWebsite ws
+
+pollWebsite :: Website -> AppConfig IO ()
+pollWebsite ws = do
+      let u   = url ws
+      let wid = idWebsite ws
+      liftIO $ putStrLn ("Polling: " ++ u)
+      h <- liftIO $ H.hash <$> scrapePage u
+      b <- DB.exec $ DB.checkWebsiteHash wid h
+      when b $ do _ <- DB.exec $ DB.updateWebsiteHash wid h
+                  liftIO $ putStrLn "Changed"
+                  return ()
+
+pollTargets :: AppConfig IO ()
+pollTargets = do 
+      ws <- DB.exec DB.getWebsites
+      mapM_ (\w -> do
+            let u   = url w
+            let wid = idWebsite w
+            ts <- DB.exec $ DB.getTargetsOnWebsite wid
+            liftIO $ putStrLn ("Polling: " ++ u ++ " with targets")
+            h <- liftIO $ H.hash <$> scrapePage u        
+            b <- DB.exec $ DB.checkWebsiteHash wid h
+            when b $ do
+                  _ <- DB.exec $ DB.updateWebsiteHash wid h
+                  mapM_ (`pollTarget` w) ts) ws
+      
+pollTarget :: Target -> Website -> AppConfig IO ()
+pollTarget t w = do
+      let (Just e) = selector t
+      let u        = url w
+      let wid      = idWebsite w
+      liftIO $ putStrLn ("Polling: " ++ u ++ " with target " ++ e)
+      h <- liftIO $ H.hash <$> scrapeElement e u
+      b <- DB.exec $ DB.checkTargetHash wid h
+      when b $ do
+            _ <- DB.exec $ DB.updateTargetHash wid h
+            liftIO $ putStrLn "Changed"
+            return ()
--- src/Main.hs
+++ src/Main.hs
@@ -1,100 +1,10 @@
 
-import Network.Wai.Handler.Warp
-import Servant
-import Servant.Auth.Server
-import Servant.Auth.Server.SetCookieOrphan ()
-import Control.Monad
-import Control.Monad.Reader
-import Control.Monad.State
 import System.Cron.Schedule
-import qualified Data.Hashable as H
+import Control.Monad.Reader
 
+import App
 import qualified DBAdapter as DB
-import Handlers.Account
-import Handlers.Targets
-import Handlers.Notification
 import Config
-import Scraper
-import Models.Website
-import Models.User
-import Models.Target
-
-type PublicAPI = LoginAPI 
-type ProtectedAPI = TargetsAPI 
-               :<|> "notification" :> NotificationAPI
-type RawFiles = Raw
-type API = PublicAPI  
-            :<|> Servant.Auth.Server.Auth '[Cookie] User :> ProtectedAPI
-            :<|> RawFiles
-
-rawFiles :: ServerT RawFiles (AppConfig Handler)
-rawFiles = serveDirectoryWebApp "www"
-
-protected :: Servant.Auth.Server.AuthResult User -> ServerT ProtectedAPI (AppConfig Handler)    
-protected (Servant.Auth.Server.Authenticated user) = hoistServer protectedAPI (`evalStateT` user) (targetServer :<|> notificationServer)
-protected _ = throwAll err401
-
-public:: ServerT PublicAPI (AppConfig Handler)
-public = accountServer
-
-server :: ServerT API (AppConfig Handler)
-server = public 
-      :<|> protected 
-      :<|> rawFiles
-
-api :: Proxy API 
-api = Proxy
-
-protectedAPI :: Proxy ProtectedAPI
-protectedAPI = Proxy 
-
-runApp :: Config -> IO ()
-runApp conf = do
-      let authApi = (Proxy :: Proxy '[CookieSettings, JWTSettings])
-      run 8080 (serveWithContext api (authConf conf) $ hoistServerWithContext api authApi (`runReaderT` conf) server)
-
-pollWebsites :: AppConfig IO ()
-pollWebsites = do 
-      ws <- DB.exec DB.getWebsites
-      mapM_ pollWebsite ws
-
-pollWebsite :: Website -> AppConfig IO ()
-pollWebsite ws = do
-      let u   = url ws
-      let wid = idWebsite ws
-      liftIO $ putStrLn ("Polling: " ++ u)
-      h <- liftIO $ H.hash <$> scrapePage u
-      b <- DB.exec $ DB.checkWebsiteHash wid h
-      when b $ do _ <- DB.exec $ DB.updateWebsiteHash wid h
-                  liftIO $ putStrLn "Changed"
-                  return ()
-
-pollTargets :: AppConfig IO ()
-pollTargets = do 
-      ws <- DB.exec DB.getWebsites
-      mapM_ (\w -> do
-            let u   = url w
-            let wid = idWebsite w
-            ts <- DB.exec $ DB.getTargetsOnWebsite wid
-            liftIO $ putStrLn ("Polling: " ++ u ++ " with targets")
-            h <- liftIO $ H.hash <$> scrapePage u        
-            b <- DB.exec $ DB.checkWebsiteHash wid h
-            when b $ do
-                  _ <- DB.exec $ DB.updateWebsiteHash wid h
-                  mapM_ (`pollTarget` w) ts) ws
-      
-pollTarget :: Target -> Website -> AppConfig IO ()
-pollTarget t w = do
-      let (Just e) = selector t
-      let u        = url w
-      let wid      = idWebsite w
-      liftIO $ putStrLn ("Polling: " ++ u ++ " with target " ++ e)
-      h <- liftIO $ H.hash <$> scrapeElement e u
-      b <- DB.exec $ DB.checkTargetHash wid h
-      when b $ do
-            _ <- DB.exec $ DB.updateTargetHash wid h
-            liftIO $ putStrLn "Changed"
-            return ()
 
 main :: IO ()
 main = do
