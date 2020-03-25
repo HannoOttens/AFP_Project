f85a6bf8ad3266a84fcbad736957ea3f18e59f90
Marcovdw <m.vandeweerthof@students.uu.nl>
2020-03-25 12:17:14 +0100

    Send notification after change

--- src/Poll.hs
+++ src/Poll.hs
@@ -6,21 +6,21 @@ import Control.Monad.Reader
 
 import qualified DBAdapter as DB
 import Config
+import Notification
 import Scraper
+import Models.Notification
 import Models.Target
 import Models.Website
 
 -- Poll a website, check for changes, update hash, return site content
 pollWebsite :: Website -> AppConfig IO (Bool, SiteContent)
-pollWebsite ws = do
-      let u   = url ws
-      let wid = idWebsite ws
-      liftIO $ putStrLn ("Polling " ++ u)
-      site <- liftIO $ getSite u
+pollWebsite w = do
+      liftIO $ putStrLn $ "Polling " ++ url w
+      site <- liftIO $ getSite $ url w
       let h = scrapePage site
-      b <- DB.exec $ DB.checkWebsiteHash wid h
+      b <- DB.exec $ DB.checkWebsiteHash (idWebsite w) h
       when b $ do -- Website changed, update hash
-            _ <- DB.exec $ DB.updateWebsiteHash wid h
+            _ <- DB.exec $ DB.updateWebsiteHash (idWebsite w) h
             return ()
       return (b, site)
 
@@ -31,24 +31,29 @@ pollTargets = do
       mapM_ (\w -> do
             (b, s) <- pollWebsite w
             when b $ do -- Website changed, continue checking targets
-                  ts <- DB.exec $ DB.getTargetsOnWebsite (idWebsite w)
-                  mapM_ (\t -> pollTarget (selector t) w s) ts) ws
+                  ts <- DB.exec $ DB.getTargetsOnWebsite $ idWebsite w
+                  mapM_ (\t -> pollTarget t w s) ts) ws
 
 -- Poll a target
-pollTarget :: Maybe Element -> Website -> SiteContent -> AppConfig IO ()
-pollTarget Nothing _ _ = do -- Full website as target, has already been checked, notify user
-      liftIO $ putStrLn "Target changed"
-      return ()
-pollTarget (Just e) w s = do -- Specified target, check if that has changed
-      liftIO $ putStrLn ("Polling " ++ url w ++ " with target " ++ e)
-      let wid = idWebsite w
-      let h   = scrapeElement (e, Nothing) s 
-      b <- DB.exec $ DB.checkTargetHash wid h
-      when b $ do -- Target changed, update hash, notify user
-            _ <- DB.exec $ DB.updateTargetHash wid h
-            liftIO $ putStrLn "Target changed"
-            return ()
+pollTarget :: Target -> Website -> SiteContent -> AppConfig IO ()
+pollTarget t w s = case selector t of
+      -- Full website as target, has already been checked, notify user
+      Nothing -> notify (userID t) (url w) $ "Website " ++ url w ++ " has changed!"
+      -- Specified target, check if that has changed
+      (Just e) -> do 
+            liftIO $ putStrLn $ "Polling " ++ url w ++ " with target " ++ e
+            let h = scrapeElement (e, Nothing) s 
+            b <- DB.exec $ DB.checkTargetHash (idWebsite w) h
+            when b $ do -- Target changed, update hash, notify user
+                  _ <- DB.exec $ DB.updateTargetHash (idWebsite w) h
+                  notify (userID t) (url w) $ "Target " ++ e ++ " on website " ++ url w ++ " has changed!"
 
 -- Return site as string
 getSite :: URL -> IO String
 getSite u = getResponseBody =<< simpleHTTP (getRequest u)
+
+-- Create a notification
+notify :: Int -> URL -> String -> AppConfig IO ()
+notify user site msg = do
+      _ <- createNotificationDetails user $ newNotification site msg
+      return ()
