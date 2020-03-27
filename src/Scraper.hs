0b0d1a1f6c8eeef565dae7c7539b243560240276
Marcovdw <m.vandeweerthof@students.uu.nl>
2020-03-27 15:41:06 +0100

    Strong types

--- src/Scraper.hs
+++ src/Scraper.hs
@@ -13,23 +13,21 @@ import Text.HTML.TagSoup.Selection
 import Text.HTML.TagSoup.Tree
 
 -- | Type synonym for String
+type Element = String
+-- | Type synonym for String
 type SiteContent = String
 -- | Type synonym for Int
-type Nesting = Int
+type Hash = Int
 
 -- | Return full site content
-scrapePage :: SiteContent -> Int
+scrapePage :: SiteContent -> Hash
 scrapePage = hash
 
 -- | Scrape to find the element hash
-scrapeElementHash :: String -> SiteContent -> Int
-scrapeElementHash sel site = hash $ scrapeElementText sel site
+scrapeElementHash :: Element -> SiteContent -> Hash
+scrapeElementHash sel site = hash $ foldl fTags "" $ scrapeElement sel site
 
--- | Scrape element text
-scrapeElementText :: String -> SiteContent -> String
-scrapeElementText sel site = foldl fTags "" $ scrapeElement sel site
-
--- | Fold tag function
+-- | Fold tags into string
 fTags :: String -> Tag String -> String
 fTags c x = case renderTags [x] of
                 "" -> c
@@ -38,7 +36,7 @@ fTags c x = case renderTags [x] of
                     _  -> c ++ "\n" ++ xs
 
 -- | Return all text within an element and optional attribute
-scrapeElement :: String -> SiteContent -> [Tag String]
+scrapeElement :: Element -> SiteContent -> [Tag String]
 scrapeElement s site = case firstResult tagTrees selector of
     Nothing  -> []
     Just res -> filter isTagText $ flattenTree [res]
